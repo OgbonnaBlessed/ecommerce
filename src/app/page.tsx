30fdae12f689780568f5a72b05eb5e6f63525c4a
Ogbonna Blessed <blessedlyrics11@gmail.com>
2025-03-15 05:35:54 +0100

    feat: add product grid component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import SalesCampaignBanner from '@/components/layout/SalesCampaignBanner'
+import ProductGrid from '@/components/product/ProductGrid';
+import { getAllProducts } from '@/sanity/lib/client';
 import React from 'react'
 
 const Home = async () => {
+  const products = await getAllProducts();
 
   return (
     <div>
       <SalesCampaignBanner />
+
+      <section className='container mx-auto py-8'>
+        <ProductGrid products={products} />
+      </section>
     </div>
   )
 }
